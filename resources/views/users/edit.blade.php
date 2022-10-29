acb791063aa68fed3639a04a638ec5b05b4e49d7
karabink <48476624+karabink@users.noreply.github.com>
2022-10-30 00:27:33 +0300

    Revert "Lab2 commit"
    This reverts commit d78891459b949b077b4c1315b7c7a06c83d2447d.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,62 +1,41 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-bold text-xl text-gray-600 leading-tight">
-            {{ __('Edit User') }}
-        </h2>
-    </x-slot>
+@extends('users.layout')
 
-    <section class="antialiased bg-gray-100 text-gray-600 px-4 pt-6">
-        <div class="flex flex-col h-full">
-            <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-                <header class="px-5 py-4 border-b border-gray-100">
-                    <h1>
-                        <a href="{{ route('users.index') }}" class="font-bold text-gray-600 hover:text-gray-400">Back</a>
-                    </h1>
-                </header>
+@section('content')
+    <h2>Edit User</h2>
+    <a href="{{ route('users.index') }}">Back</a>
+    <br /><br />
 
-                <form class="m-4 flex flex-col" action="{{ route('users.update', $user->id )}}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <div class="block m-3">
-                        <label for="name" class="block text-sm font-medium text-gray-800 ml-1 mt-5">Name</label>
-                        <div class="relative mt-1 rounded-md shadow-sm">
-                            <input type="text" name="name" id="name" value="{{ $user->name }}" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Name">
-                        </div>
+    <form action="{{ route('users.update', $user->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <strong>Name:</strong>
+        <input type="text" name="name" value="{{ $user->name }}" class="form-control" placeholder="Name">
+        <br />
 
-                        @error('name')
-                        <span style="color: red">{{ $message }}</span>
-                        @enderror
-                    </div>
+        @error('name')
+        <span style="color: red">{{ $message }}</span>
+        @enderror
+        <br />
 
-                    <div class="block m-3">
-                        <label for="email" class="block text-sm font-medium text-gray-800 ml-1 mt-5">Email</label>
-                        <div class="relative mt-1 rounded-md shadow-sm">
-                            <input type="text" name="email" id="email" value="{{ $user->email }}" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Email">
-                        </div>
+        <strong>Email:</strong>
+        <input type="text" name="email" value="{{ $user->email }}" placeholder="Email" />
+        <br />
 
-                        @error('password')
-                        <span style="color: red">{{ $message }}</span>
-                        @enderror
-                    </div>
+        @error('email')
+        <span style="color: red">{{ $message }}</span>
+        @enderror
+        <br />
 
-                    <div class="block m-3">
-                        <label for="email" class="block text-sm font-medium text-gray-800 ml-1 mt-5">Password</label>
-                        <div class="relative mt-1 rounded-md shadow-sm">
-                            <input type="password" name="password" id="password" value="{{ $user->password }}" class="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Password">
-                        </div>
+        <strong>Password:</strong>
+        <input type="password" name="password" value="{{ $user->password }}" placeholder="Password" />
+        <br />
 
-                        @error('password')
-                        <span style="color: red">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="m-5 flex flex-row-reverse">
-                        <button class="bg-transparent hover:bg-gray-500 text-gray-600-700 font-semibold hover:text-white py-2 px-4 border border-gray-600 hover:border-transparent rounded object-right" type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        </div>
-        </div>
-    </section>
+        @error('password')
+        <span style="color: red">{{ $message }}</span>
+        @enderror
+        <br /><br />
 
-</x-app-layout>
+        <button type="submit">Submit</button>
+        <br /><br /><br />
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('users', UserController::class)
-->middleware('auth');
+//Route::resource('users', UserController::class);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/users', [UserController::class, 'index'])
+    ->name('users.index');
 
-require __DIR__.'/auth.php';
+Route::get('/users/create', [UserController::class, 'create'])
+    ->name('users.create');
+
+Route::post('/users/', [UserController::class, 'store'])
+    ->name('users.store');
+
+Route::get('/users/{user}', [UserController::class, 'show'])
+    ->name('users.show');
+
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])
+    ->name('users.edit');
+
+Route::put('/users/{user}', [UserController::class, 'update'])
+    ->name('users.update');
+
+Route::delete('/users/{user}', [UserController::class, 'destroy'])
+    ->name('users.destroy');
